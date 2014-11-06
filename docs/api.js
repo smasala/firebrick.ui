ea46fcbe8cdbcff10a9ac1907370bf0677eb25b0
smasala <me@smasala.com>
2014-11-06 10:03:29 +0100

    docs

---
+++ docs/api.js
@@ -0,0 +1,56 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Firebrick.ui",
+        "Firebrick.ui.components.button.Button",
+        "Firebrick.ui.components.button.ToggleButton",
+        "Firebrick.ui.components.common.Base",
+        "Firebrick.ui.components.common.MultiplesBase",
+        "Firebrick.ui.components.containers.Base",
+        "Firebrick.ui.components.containers.Form",
+        "Firebrick.ui.components.containers.Grid",
+        "Firebrick.ui.components.containers.GridColumn",
+        "Firebrick.ui.components.containers.Panel",
+        "Firebrick.ui.components.display.Header",
+        "Firebrick.ui.components.display.Text",
+        "Firebrick.ui.components.fields.Base",
+        "Firebrick.ui.components.fields.Checkbox",
+        "Firebrick.ui.components.fields.Email",
+        "Firebrick.ui.components.fields.Input",
+        "Firebrick.ui.components.fields.Radio",
+        "Firebrick.ui.components.fields.SelectBox",
+        "Firebrick.ui.components.fields.TextArea",
+        "Firebrick.ui.components.table.Table",
+        "Firebrick.ui.components.table.TreeTable",
+        "Firebrick.ui.utils",
+        "plugins.RadioList"
+    ],
+    "modules": [
+        "Firebrick",
+        "Firebrick.ui",
+        "Firebrick.ui.components",
+        "plugins"
+    ],
+    "allModules": [
+        {
+            "displayName": "Firebrick",
+            "name": "Firebrick",
+            "description": "Firebrick component library for Firebrick JS MVC"
+        },
+        {
+            "displayName": "Firebrick.ui",
+            "name": "Firebrick.ui",
+            "description": "util methods"
+        },
+        {
+            "displayName": "Firebrick.ui.components",
+            "name": "Firebrick.ui.components",
+            "description": "Extends: {{#crossLink \"Firebrick.ui.components.common.Base\"}}{{/crossLink}}"
+        },
+        {
+            "displayName": "plugins",
+            "name": "plugins"
+        }
+    ]
+} };
+});
